8bae25645049556cb4a99cae51509a91a1a1ca78
PaaTay2.0 <macbookpro@macbooks-mbp.myfiosgateway.com>
2020-06-15 22:15:38 -0400

    Navbar Component

---
+++ src/Component/NavComponent.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
+
+const MainNav = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="danger" light expand="md">
+                <NavbarBrand href="/">AYO MUSIC</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="#">HOME</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">PRICING</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">DOWNLOAD</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">CONTACT</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">LOGIN</NavLink>
+                        </NavItem>
+                        <NavbarText>|</NavbarText>
+                         <NavItem>
+                            <NavLink href="#">SIGN UP</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default MainNav;
